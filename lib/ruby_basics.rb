96c14a4a446b3c66646771f8cae646bcc6ee78f4
Danny Wakeling <wakeling101@hotmail.com>
2019-02-18 10:32:57 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,26 +1,32 @@
 def division(num1, num2)
- 
+ num2/num1
 end
+
 
 def assign_variable(value)
   
 end
 
+
 def argue
   
 end
+
 
 def greeting
 
 end
 
+
 def return_a_value
 
 end
 
+
 def last_evaluated_value
 
 end
+
 
 def pizza_party
 
